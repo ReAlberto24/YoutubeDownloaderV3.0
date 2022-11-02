0abbb2cbaf517d02b48a2cf03ff61ba96f617e26
ReAlberto24 <albert.to.trebla2@gmail.com>
2022-11-02 21:36:45 +0100

    Moved 7z to another dir

--- download_ffmpeg.bat
+++ download_ffmpeg.bat
@@ -2,7 +2,7 @@
 echo Downloading ffmpeg
 curl https://www.gyan.dev/ffmpeg/builds/packages/ffmpeg-2022-10-30-git-ed5a438f05-full_build.7z --output ffmpeg-git-full.7z --ssl-no-revoke
 echo Decompressing ffmpeg
-.\7z.exe x .\ffmpeg-git-full.7z
+.\7z\7z.exe x .\ffmpeg-git-full.7z
 echo Moving ffmpeg
 move .\ffmpeg-2022-10-30-git-ed5a438f05-full_build\bin\ffmpeg.exe .\ffmpeg.exe
 echo Removing temp files
